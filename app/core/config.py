623ff359aead130834a4288b21817dc2b10ef80a
moltycore <idristan97@gmail.com>
2026-03-25 21:40:21 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -6,5 +6,8 @@ load_dotenv()
 
 GROQ_KEY = os.getenv("GROQ_API_KEY")
 COHERE_KEY = os.getenv("COHERE_API_KEY")
-DEEPSEEK_KEY = os.getenv("DEEPSEEK_API_KEY")
 HF_KEY = os.getenv("HF_API_KEY")
+
+# Yeni Hibrit Yapı (DeepSeek emekliye ayrıldı)
+OPENROUTER_KEY = os.getenv("OPENROUTER_API_KEY")
+PUTER_APP_ID = os.getenv("PUTER_APP_ID")
